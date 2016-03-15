57beb5505b383fa893ed596bdc5719e0328963b8
hcc <hcc@SALLTP150007>
2016-03-15 23:55:53 +0800

    上传

--- haha_eureka/haha_eureka/src/main/java/com/hui/HahaEurekaApplication.java
+++ haha_eureka/haha_eureka/src/main/java/com/hui/HahaEurekaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HahaEurekaApplication {
-
+	//dsad
 	public static void main(String[] args) {
 		SpringApplication.run(
 				HahaEurekaApplication.class, args);
